6002ad009f88ee089b060fc55a7542191df0e66f
cocomozila <joseyun94@naver.com>
2024-04-16 22:05:29 +0900

    feat: UserController 추가

--- src/main/java/com/wow/delivery/controller/UserController.java
+++ src/main/java/com/wow/delivery/controller/UserController.java
@@ -1,7 +1,26 @@
 package com.wow.delivery.controller;
 
+import com.wow.delivery.dto.user.UserSignupDTO;
+import com.wow.delivery.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<HttpStatus> signup(@RequestBody @Valid UserSignupDTO userSignupDTO) {
+        userService.signup(userSignupDTO.toEntity());
+        return ResponseEntity.ok(HttpStatus.CREATED);
+    }
 }
