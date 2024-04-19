e3fe4e6da91b17022bc5ce6afb23dd505e9b2912
cocomozila <joseyun94@naver.com>
2024-04-20 00:44:59 +0900

    feat: @JsonProperty 명시

--- src/main/java/com/wow/delivery/error/ErrorResponse.java
+++ src/main/java/com/wow/delivery/error/ErrorResponse.java
@@ -1,13 +1,19 @@
 package com.wow.delivery.error;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 public class ErrorResponse {
 
+    @JsonProperty("status")
     private HttpStatus status;
+
+    @JsonProperty("code")
     private String code;
+
+    @JsonProperty("message")
     private String message;
 
     public ErrorResponse(ErrorCode errorCode) {
