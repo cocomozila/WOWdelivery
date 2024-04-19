6147555f0e6260b099192030454a260388493761
cocomozila <joseyun94@naver.com>
2024-04-19 18:11:00 +0900

    refactor: 기존 ExceptionController 수정

--- src/main/java/com/wow/delivery/error/ExceptionController.java
+++ src/main/java/com/wow/delivery/error/ExceptionController.java
@@ -1,33 +1,21 @@
 package com.wow.delivery.error;
 
-import com.wow.delivery.error.exception.DuplicateEmailException;
-import com.wow.delivery.error.exception.DuplicatePhoneNumberException;
-import com.wow.delivery.error.exception.InvalidCredentialsException;
-import org.springframework.http.HttpStatus;
+import com.wow.delivery.error.exception.DuplicateException;
+import com.wow.delivery.error.exception.MismatchException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 @RestControllerAdvice
 public class ExceptionController {
 
-    private String getSimpleName(Exception e) {
-        return e.getClass().getSimpleName();
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateException(DuplicateException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode()), e.getErrorCode().getStatus());
     }
 
-    @ExceptionHandler(DuplicateEmailException.class)
-    public ErrorMessage handleDuplicateEmailException(DuplicateEmailException e) {
-        return new ErrorMessage(e.getLocalizedMessage(), getSimpleName(e));
-    }
-
-    @ExceptionHandler(DuplicatePhoneNumberException.class)
-    public ErrorMessage handleDuplicatePhoneNumberException(DuplicatePhoneNumberException e) {
-        return new ErrorMessage(e.getLocalizedMessage(), getSimpleName(e));
-    }
-
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ErrorMessage handleInvalidCredentialsException(InvalidCredentialsException e) {
-        return new ErrorMessage(e.getLocalizedMessage(), getSimpleName(e));
+    @ExceptionHandler(MismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMismatchException(MismatchException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode()), e.getErrorCode().getStatus());
     }
 }
