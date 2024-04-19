896b8c9d0bf5f22e728422b407040dcc383469cc
cocomozila <joseyun94@naver.com>
2024-04-20 00:46:00 +0900

    refactor: UserRepository 중복기능 공통처리

--- src/main/java/com/wow/delivery/repository/UserRepository.java
+++ src/main/java/com/wow/delivery/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.wow.delivery.repository;
 
 import com.wow.delivery.entity.User;
+import com.wow.delivery.error.ErrorCode;
+import com.wow.delivery.error.exception.MismatchException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
         boolean existsByPhoneNumber(String phoneNumber);
 
         Optional<User> findUserByEmail(String email);
+
+        default User getUserByEmail(String email) {
+                return findUserByEmail(email)
+                    .orElseThrow(()-> new MismatchException(ErrorCode.MISMATCH_ACCOUNT));
+        }
 }
