57b58ad9f6addc086e10dbf21ffcdac1e6e30c75
cocomozila <joseyun94@naver.com>
2024-04-24 21:37:03 +0900

    refactor: UserSignupDTO 정규표현식 추가

--- src/main/java/com/wow/delivery/dto/user/UserSignupDTO.java
+++ src/main/java/com/wow/delivery/dto/user/UserSignupDTO.java
@@ -2,21 +2,37 @@ package com.wow.delivery.dto.user;
 
 import com.wow.delivery.entity.User;
 import com.wow.delivery.util.PasswordEncoder;
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class UserSignupDTO {
 
-    @Email
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$")
     private String email;
 
-    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 16)
     private String password;
 
     @NotNull
+    @Pattern(regexp = "^01(?:0|1|[6-9])(\\d{3}|\\d{4})(\\d{4})$")
     private String phoneNumber;
+
+    public UserSignupDTO() {
+    }
+
+    @Builder
+    public UserSignupDTO(String email, String password, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
 
     public User toEntity() {
         PasswordEncodingDTO passwordEncoder = PasswordEncoder.encodePassword(this.password);
