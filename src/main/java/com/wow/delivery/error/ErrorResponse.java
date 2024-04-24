2b58e8fd1e8656b5003d5cc05c4bf0a43d7f8479
cocomozila <joseyun94@naver.com>
2024-04-24 21:49:35 +0900

    feat: BindResult 예외처리 추가

--- src/main/java/com/wow/delivery/error/ErrorResponse.java
+++ src/main/java/com/wow/delivery/error/ErrorResponse.java
@@ -1,14 +1,14 @@
 package com.wow.delivery.error;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wow.delivery.error.exception.CustomException;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.List;
 
 @Getter
 public class ErrorResponse {
-
-    @JsonProperty("status")
-    private HttpStatus status;
 
     @JsonProperty("code")
     private String code;
@@ -16,9 +16,25 @@ public class ErrorResponse {
     @JsonProperty("message")
     private String message;
 
-    public ErrorResponse(ErrorCode errorCode) {
-        this.status = errorCode.getStatus();
-        this.code = errorCode.getCode();
+    @JsonProperty("errors")
+    private List<BindingError> bindingErrors;
+
+    private ErrorResponse(CustomException e) {
+        this.code = e.getClass().getSimpleName();
+        this.message = e.getErrorCode().getMessage();
+    }
+
+    private ErrorResponse(MethodArgumentNotValidException e, ErrorCode errorCode) {
+        this.code = e.getClass().getSimpleName();
         this.message = errorCode.getMessage();
+        this.bindingErrors = BindingError.of(e.getBindingResult());
+    }
+
+    public static ErrorResponse of(CustomException e) {
+        return new ErrorResponse(e);
+    }
+
+    public static ErrorResponse of(MethodArgumentNotValidException e, ErrorCode errorCode) {
+        return new ErrorResponse(e, errorCode);
     }
 }
