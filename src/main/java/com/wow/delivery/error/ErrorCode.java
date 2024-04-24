7a66bd6f3a08a8b100727fe36507898798d2e9da
cocomozila <joseyun94@naver.com>
2024-04-24 21:47:27 +0900

    refactor: ErrorCode status, code 삭제

--- src/main/java/com/wow/delivery/error/ErrorCode.java
+++ src/main/java/com/wow/delivery/error/ErrorCode.java
@@ -1,27 +1,23 @@
 package com.wow.delivery.error;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Getter
 public enum ErrorCode {
 
     // DuplicateException
-    DUPLICATE_EMAIL(BAD_REQUEST, "중복", "중복된 이메일입니다."),
-    DUPLICATE_PHONE_NUMBER(BAD_REQUEST, "중복", "중복된 휴대폰 번호입니다."),
+    DUPLICATE_EMAIL("중복된 이메일입니다."),
+    DUPLICATE_PHONE_NUMBER("중복된 휴대폰 번호입니다."),
 
-    // MismatchException
-    MISMATCH_ACCOUNT(BAD_REQUEST, "불일치", "이메일이나 비밀번호가 일치하지 않습니다.");
+    // DataNotFoundException
+    MISMATCH_ACCOUNT("일치하는 계정을 찾을 수 없습니다."),
 
-    private final HttpStatus status;
-    private final String code;
+    // Parameter
+    INVALID_PARAMETER("유효하지 않은 입력 값입니다.");
+
     private final String message;
 
-    ErrorCode(HttpStatus status, String code, String message) {
-        this.status = status;
-        this.code = code;
+    ErrorCode(String message) {
         this.message = message;
     }
 }
