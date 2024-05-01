bfa4cc7faf1a3b33176f26d44609510fa997cd5d
cocomozila <joseyun94@naver.com>
2024-05-02 01:15:48 +0900

    Refactor: CustomException 어노테이션 추가

--- src/main/java/com/wow/delivery/error/exception/CustomException.java
+++ src/main/java/com/wow/delivery/error/exception/CustomException.java
@@ -2,11 +2,13 @@ package com.wow.delivery.error.exception;
 
 import com.wow.delivery.error.ErrorCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
 
-    public CustomException(final ErrorCode errorCode) {
-        super(errorCode.getMessage());
-    }
+    private final ErrorCode errorCode;
+
+
 }
