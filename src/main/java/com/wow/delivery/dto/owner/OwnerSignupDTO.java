3ac6a06eede63e58cf6ecac738ae472322714118
cocomozila <joseyun94@naver.com>
2024-05-03 03:05:08 +0900

    feat: OwnerSigninDTO 추가

---
+++ src/main/java/com/wow/delivery/dto/owner/OwnerSignupDTO.java
@@ -0,0 +1,46 @@
+package com.wow.delivery.dto.owner;
+
+import com.wow.delivery.dto.common.PasswordEncodingDTO;
+import com.wow.delivery.entity.Owner;
+import com.wow.delivery.util.PasswordEncoder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class OwnerSignupDTO {
+
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$")
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 16)
+    private String password;
+
+    @NotNull
+    @Pattern(regexp = "^01(?:0|1|[6-9])(\\d{3}|\\d{4})(\\d{4})$")
+    private String phoneNumber;
+
+    public OwnerSignupDTO() {
+    }
+
+    @Builder
+    public OwnerSignupDTO(String email, String password, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Owner toEntity() {
+        PasswordEncodingDTO passwordEncoder = PasswordEncoder.encodePassword(this.password);
+        return Owner.builder().email(this.email)
+            .password(passwordEncoder.getEncodePassword())
+            .salt(passwordEncoder.getSalt())
+            .phoneNumber(this.phoneNumber)
+            .build();
+    }
+}
