33042023267e04b12b312dbfc3142bb93074f71c
cocomozila <joseyun94@naver.com>
2024-05-03 03:05:26 +0900

    feat: OwnerController 추가

---
+++ src/main/java/com/wow/delivery/controller/OwnerController.java
@@ -0,0 +1,39 @@
+package com.wow.delivery.controller;
+
+import com.wow.delivery.dto.owner.OwnerSigninDTO;
+import com.wow.delivery.dto.owner.OwnerSignupDTO;
+import com.wow.delivery.service.OwnerService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/owner")
+@RequiredArgsConstructor
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<HttpStatus> signup(@RequestBody @Valid OwnerSignupDTO ownerSignupDTO) {
+        ownerService.signup(ownerSignupDTO.toEntity());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<HttpStatus> signin(@RequestBody @Valid OwnerSigninDTO ownerSigninDTO, HttpSession session) {
+        ownerService.signin(ownerSigninDTO.getEmail(), ownerSigninDTO.getPassword(), session);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpSession session) {
+        ownerService.logout(session);
+    }
+}
