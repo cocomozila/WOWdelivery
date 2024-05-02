616dbad636763d99a4c534f46f74a6202e38b578
cocomozila <joseyun94@naver.com>
2024-05-03 03:04:02 +0900

    feat: Owner Entity 추가

---
+++ src/main/java/com/wow/delivery/entity/Owner.java
@@ -0,0 +1,36 @@
+package com.wow.delivery.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(uniqueConstraints = { @UniqueConstraint(name = "email_phoneNumber_unique", columnNames = { "email", "phone_number" }) })
+public class Owner extends BaseEntity {
+
+    @Column(name = "email", columnDefinition = "VARCHAR(30)", nullable = false)
+    private String email;
+
+    @Column(name = "password", columnDefinition = "VARCHAR(100)", nullable = false)
+    private String password;
+
+    @Column(name = "salt", columnDefinition = "VARCHAR(100)", nullable = false)
+    private String salt;
+
+    @Column(name = "phone_number", columnDefinition = "VARCHAR(20)", nullable = false)
+    private String phoneNumber;
+
+    @Builder
+    public Owner(String email, String password, String salt, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.salt = salt;
+        this.phoneNumber = phoneNumber;
+    }
+}
