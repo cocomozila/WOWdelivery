d4c583cd25c16b64122002b0b0665089c384986a
cocomozila <joseyun94@naver.com>
2024-05-02 15:31:53 +0900

    feat: UserServiceTest 로그인 기능 테스트 추가

--- src/test/java/com/wow/delivery/service/UserServiceTest.java
+++ src/test/java/com/wow/delivery/service/UserServiceTest.java
@@ -2,11 +2,14 @@ package com.wow.delivery.service;
 
 import com.wow.delivery.dto.user.UserSignupDTO;
 import com.wow.delivery.entity.User;
+import com.wow.delivery.error.exception.DataNotFoundException;
 import com.wow.delivery.error.exception.InvalidParameterException;
 import com.wow.delivery.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -80,5 +83,50 @@ class UserServiceTest {
 
         Assertions.assertThrows(InvalidParameterException.class,
             () -> userService.signup(duplicatePhoneNumberSignupUser));
+    }
+
+    @Test
+    @DisplayName("로그인 성공 - 세션에 이메일 등록")
+    void signinSuccessTest() {
+        UserSignupDTO signupUser = UserSignupDTO.builder()
+            .email("test@gmail.com")
+            .password("12345678")
+            .phoneNumber("01011111111")
+            .build();
+        userService.signup(signupUser.toEntity());
+
+        HttpSession session = new MockHttpSession();
+        userService.signin(signupUser.getEmail(), signupUser.getPassword(), session);
+        assertThat(session.getAttribute("userEmail")).isEqualTo(signupUser.getEmail());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 일치하지 않는 이메일")
+    void signinFailTest_invalidEmail() {
+        UserSignupDTO signupUser = UserSignupDTO.builder()
+            .email("test@gmail.com")
+            .password("12345678")
+            .phoneNumber("01011111111")
+            .build();
+        userService.signup(signupUser.toEntity());
+
+        HttpSession session = new MockHttpSession();
+        Assertions.assertThrows(DataNotFoundException.class,
+            () -> userService.signin("fail@gmail.com", signupUser.getPassword(), session));
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 일치하지 않는 패스워드")
+    void signinFailTest_invalidPassword() {
+        UserSignupDTO signupUser = UserSignupDTO.builder()
+            .email("test@gmail.com")
+            .password("12345678")
+            .phoneNumber("01011111111")
+            .build();
+        userService.signup(signupUser.toEntity());
+
+        HttpSession session = new MockHttpSession();
+        Assertions.assertThrows(DataNotFoundException.class,
+            () -> userService.signin(signupUser.getEmail(), "87654321", session));
     }
 }
