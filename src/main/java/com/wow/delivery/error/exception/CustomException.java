235547aa52fcf4c80b9f911f0699eb7a35aaa90b
cocomozila <joseyun94@naver.com>
2024-05-07 16:46:20 +0900

    refactor: CustomException 수정
    - 생성자 및 필드 변경

--- src/main/java/com/wow/delivery/error/exception/CustomException.java
+++ src/main/java/com/wow/delivery/error/exception/CustomException.java
@@ -2,13 +2,15 @@ package com.wow.delivery.error.exception;
 
 import com.wow.delivery.error.ErrorCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
+    public CustomException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
 
+    }
 }
