11241a62a28d0762c2d4fd4a0e25bad3f034876d
cocomozila <joseyun94@naver.com>
2024-05-07 17:30:51 +0900

    Refactor: UserRepository
    - CustomJpaRepository 상속

--- src/main/java/com/wow/delivery/repository/UserRepository.java
+++ src/main/java/com/wow/delivery/repository/UserRepository.java
@@ -1,21 +1,7 @@
 package com.wow.delivery.repository;
 
 import com.wow.delivery.entity.User;
-import com.wow.delivery.error.ErrorCode;
-import com.wow.delivery.error.exception.DataNotFoundException;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface UserRepository extends CustomJpaRepository<User, Long> {
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-        boolean existsByEmail(String email);
-        boolean existsByPhoneNumber(String phoneNumber);
-
-        Optional<User> findUserByEmail(String email);
-
-        default User getUserByEmail(String email) {
-                return findUserByEmail(email)
-                    .orElseThrow(()-> new DataNotFoundException(ErrorCode.MISMATCH_ACCOUNT));
-        }
 }
