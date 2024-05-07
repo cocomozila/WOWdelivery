345fe94d4f9821e342298fb030c8d40540f80e0d
cocomozila <joseyun94@naver.com>
2024-05-07 16:45:32 +0900

    refactor: ErrorCode 수정
    - 데이터 간략화

--- src/main/java/com/wow/delivery/error/ErrorCode.java
+++ src/main/java/com/wow/delivery/error/ErrorCode.java
@@ -6,11 +6,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     // DuplicateException
-    DUPLICATE_EMAIL("중복된 이메일입니다."),
-    DUPLICATE_PHONE_NUMBER("중복된 휴대폰 번호입니다."),
+    DUPLICATE_DATA("중복된 데이터 입니다."),
 
     // DataNotFoundException
-    MISMATCH_ACCOUNT("일치하는 계정을 찾을 수 없습니다."),
+    DATA_NOT_FOUND("데이터를 찾을 수 없습니다."),
 
     // Parameter
     INVALID_PARAMETER("유효하지 않은 입력 값입니다.");
