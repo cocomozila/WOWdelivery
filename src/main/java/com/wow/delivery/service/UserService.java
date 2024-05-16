ad8382fe68032e618fed7f336db9303284a6cf07
cocomozila <joseyun94@naver.com>
2024-05-16 20:46:27 +0900

    Refactor: UserService, UserRepository
    - UserService 수정
      메서드 파라미터 변경 Entity > DTO
      그에 따른 변경점 수정
    - UserRepository

--- src/main/java/com/wow/delivery/service/UserService.java
+++ src/main/java/com/wow/delivery/service/UserService.java
@@ -1,5 +1,8 @@
 package com.wow.delivery.service;
 
+import com.wow.delivery.dto.common.PasswordEncodingDTO;
+import com.wow.delivery.dto.user.UserSigninDTO;
+import com.wow.delivery.dto.user.UserSignupDTO;
 import com.wow.delivery.entity.User;
 import com.wow.delivery.error.ErrorCode;
 import com.wow.delivery.error.exception.DataNotFoundException;
@@ -11,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -18,16 +23,23 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void signup(User user) {
-        validDuplicateUser(user);
+    public void signup(UserSignupDTO userSignupDTO) {
+        validDuplicateUser(userSignupDTO);
+        PasswordEncodingDTO passwordEncoder =
+            PasswordEncoder.encodePassword(userSignupDTO.getPassword());
+        User user = User.builder()
+                .email(userSignupDTO.getEmail())
+                .password(passwordEncoder.getEncodePassword())
+                .salt(passwordEncoder.getSalt())
+                .phoneNumber(userSignupDTO.getPhoneNumber())
+                .build();
         userRepository.save(user);
     }
 
     @Transactional(readOnly = true)
-    public void signin(String email, String password, HttpSession session) {
-        User findUser = userRepository.getByEmail(email);
-
-        if (!PasswordEncoder.matchesPassword(password, findUser.getPassword(), findUser.getSalt())) {
+    public void signin(UserSigninDTO userSigninDTO, HttpSession session) {
+        User findUser = userRepository.getByEmail(userSigninDTO.getEmail());
+        if (!PasswordEncoder.matchesPassword(userSigninDTO.getPassword(), findUser.getPassword(), findUser.getSalt())) {
             throw new DataNotFoundException(ErrorCode.DATA_NOT_FOUND, "일치하는 계정을 찾을 수 없습니다.");
         }
         setSession(findUser, session);
@@ -38,15 +50,14 @@ public class UserService {
     }
 
     private void setSession(User user, HttpSession session) {
-        session.setAttribute("userEmail", user.getEmail());
-        session.setMaxInactiveInterval(60 * 30);
+        session.setAttribute(UUID.randomUUID().toString(), user);
     }
 
-    private void validDuplicateUser(User user) {
-        if (isDuplicateEmail(user.getEmail())) {
+    private void validDuplicateUser(UserSignupDTO userSignupDTO) {
+        if (isDuplicateEmail(userSignupDTO.getEmail())) {
             throw new InvalidParameterException(ErrorCode.DUPLICATE_DATA, "중복된 이메일 입니다.");
         }
-        if (isDuplicatePhoneNumber(user.getPhoneNumber())) {
+        if (isDuplicatePhoneNumber(userSignupDTO.getPhoneNumber())) {
             throw new InvalidParameterException(ErrorCode.DUPLICATE_DATA, "중복된 휴대폰 번호 입니다.");
         }
     }
