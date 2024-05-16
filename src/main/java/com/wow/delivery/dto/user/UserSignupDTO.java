6bf3b3585bbcd2c338036d7b700c0b0989b053d2
cocomozila <joseyun94@naver.com>
2024-05-16 20:40:08 +0900

    Refactor: UserController, DTO 수정
    - UserController
      [] 기존 entity 변환 로직 삭제
    - UserSignupDTO toEntity 메서드 삭제
    - UserSigninDTO 생성자 Builder패턴 추가

--- src/main/java/com/wow/delivery/dto/user/UserSignupDTO.java
+++ src/main/java/com/wow/delivery/dto/user/UserSignupDTO.java
@@ -1,8 +1,5 @@
 package com.wow.delivery.dto.user;
 
-import com.wow.delivery.dto.common.PasswordEncodingDTO;
-import com.wow.delivery.entity.User;
-import com.wow.delivery.util.PasswordEncoder;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -34,14 +31,4 @@ public class UserSignupDTO {
         this.password = password;
         this.phoneNumber = phoneNumber;
     }
-
-    public User toEntity() {
-        PasswordEncodingDTO passwordEncoder = PasswordEncoder.encodePassword(this.password);
-        return User.builder().email(this.email)
-                .password(passwordEncoder.getEncodePassword())
-                .salt(passwordEncoder.getSalt())
-                .phoneNumber(this.phoneNumber)
-                .build();
-    }
-
 }
