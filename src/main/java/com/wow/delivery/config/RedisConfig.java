7db161242f82a2758b3d6483f846d6365d19e0d6
cocomozila <joseyun94@naver.com>
2024-05-16 20:50:25 +0900

    feat: Redis Session 추가
    - User
      Serializable 구현
    - RedisConfig
      @EnableRedisHttpSession Redis 세션관리 추가
    - application.yml
      Redis 설정 추가
    - build.gradle
      Redis 종속성 추가

---
+++ src/main/java/com/wow/delivery/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.wow.delivery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
