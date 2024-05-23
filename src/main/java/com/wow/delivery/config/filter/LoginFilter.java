2684fc5e054f8d5c907fa2f7b6de7a8a96ec6d67
cocomozila <joseyun94@naver.com>
2024-05-23 17:24:25 +0900

    feat: 로그인 session 기능 추가
    - LoginFilter 추가
    - ExceptionHandlerFilter: Filter 레이어의 전역 예외처리
    - WebConfig: Filter 설정
    - SessionConfig: 쿠키명 설정
    - AuthenticationException: Custom 예외클래스 추가

---
+++ src/main/java/com/wow/delivery/config/filter/LoginFilter.java
@@ -0,0 +1,66 @@
+package com.wow.delivery.config.filter;
+
+import com.wow.delivery.error.ErrorCode;
+import com.wow.delivery.error.exception.AuthenticationException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+
+    private static final String COOKIE_NAME = "DELIVERY";
+    private static final String[] whitelist = {"/user/signin", "/user/signup", "/owner/signin", "owner/signup"};
+
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        ServletContext context = filterConfig.getServletContext();
+        redisTemplate = WebApplicationContextUtils.getRequiredWebApplicationContext(context)
+            .getBean(StringRedisTemplate.class);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String requestURI = request.getRequestURI();
+
+        if (isLoginCheckPath(requestURI)) {
+            String cookieValue = getCookieValue(request);
+            HttpSession session = request.getSession(false);
+
+            if (cookieValue == null || session == null) {
+                throw new AuthenticationException(ErrorCode.UNAUTHENTICATED, "로그인이 필요합니다.");
+            }
+
+            String sessionKey = "spring:session:sessions:" + cookieValue;
+            if (!redisTemplate.hasKey(sessionKey)) {
+                throw new AuthenticationException(ErrorCode.UNAUTHENTICATED, "로그인이 필요합니다.");
+            }
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isLoginCheckPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
+
+    private String getCookieValue(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(COOKIE_NAME)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
