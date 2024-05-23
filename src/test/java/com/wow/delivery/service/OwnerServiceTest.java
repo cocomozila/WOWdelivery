b9db7665225b63b464687ae2ef72f11f1123c43f
cocomozila <joseyun94@naver.com>
2024-05-23 17:27:45 +0900

    refactor: OwnerServiceTest
    - 단위 테스트코드 수정

--- src/test/java/com/wow/delivery/service/OwnerServiceTest.java
+++ src/test/java/com/wow/delivery/service/OwnerServiceTest.java
@@ -1,151 +1,240 @@
 package com.wow.delivery.service;
 
+import com.wow.delivery.dto.common.PasswordEncodingDTO;
+import com.wow.delivery.dto.owner.OwnerSigninDTO;
 import com.wow.delivery.dto.owner.OwnerSignupDTO;
 import com.wow.delivery.entity.Owner;
 import com.wow.delivery.error.exception.DataNotFoundException;
 import com.wow.delivery.error.exception.InvalidParameterException;
 import com.wow.delivery.repository.OwnerRepository;
+import com.wow.delivery.util.PasswordEncoder;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-class OwnerServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class OwnerServiceTest {
 
-    @Autowired
-    OwnerService ownerService;
+    @InjectMocks
+    private OwnerService ownerService;
 
-    @Autowired
-    OwnerRepository ownerRepository;
+    @Spy
+    private OwnerRepository ownerRepository;
 
     @AfterEach
     void after() {
-        ownerRepository.deleteAll();
+        reset(ownerRepository);
     }
 
-    @Test
-    @DisplayName("가게사장 회원가입 성공")
-    void ownerSignupSuccessTest() {
-        Owner signupOwner = OwnerSignupDTO.builder()
-            .email("owner1@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build().toEntity();
+    @Nested
+    @DisplayName("회원가입")
+    class Signup {
 
-        ownerService.signup(signupOwner);
-        Owner findOwner = ownerRepository.getOwnerByEmail(signupOwner.getEmail());
-        assertThat(findOwner.getId()).isNotNull();
+        @Test
+        @DisplayName("성공")
+        void signupSuccessTest() {
+            // given
+            OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
+                .email("test@gmail.com")
+                .password("12345678")
+                .phoneNumber("01011111111")
+                .build();
+
+            given(ownerRepository.existsByEmail(signupOwner.getEmail()))
+                .willReturn(false);
+            given(ownerRepository.existsByPhoneNumber(signupOwner.getPhoneNumber()))
+                .willReturn(false);
+
+            // when
+            ownerService.signup(signupOwner);
+
+            // then
+            then(ownerRepository)
+                .should(times(1))
+                .save(any());
+        }
+
+        @Test
+        @DisplayName("실패 - 중복된 이메일")
+        void signupFailTest_DuplicateEmail() {
+            // given
+            OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
+                .email("test@gmail.com")
+                .password("12345678")
+                .phoneNumber("01011111111")
+                .build();
+
+            given(ownerRepository.existsByEmail(any()))
+                .willReturn(true);
+
+
+            // when & then
+            assertThrows(InvalidParameterException.class,
+                () -> ownerService.signup(signupOwner));
+        }
+
+        @Test
+        @DisplayName("실패 - 중복된 휴대폰 번호")
+        void signupFailTest_DuplicatePhoneNumber() {
+            // given
+            OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
+                .email("test@gmail.com")
+                .password("12345678")
+                .phoneNumber("01011111111")
+                .build();
+
+            given(ownerRepository.existsByEmail(any()))
+                .willReturn(false);
+            given(ownerRepository.existsByPhoneNumber(any()))
+                .willReturn(true);
+
+            // when & then
+            assertThrows(InvalidParameterException.class,
+                () -> ownerService.signup(signupOwner));
+        }
     }
 
-    @Test
-    @DisplayName("가게사장 회원가입 실패 - 중복된 이메일")
-    void signupFailTest_DuplicateEmail() {
-        Owner signupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build().toEntity();
+    @Nested
+    @DisplayName("로그인")
+    class Signin {
 
-        ownerService.signup(signupOwner);
+        @Test
+        @DisplayName("성공 - 세션에 이메일 등록")
+        void signinSuccessTest() {
+            // given
+            OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
+                .email("test@gmail.com")
+                .password("12345678")
+                .phoneNumber("01011112222")
+                .build();
 
-        Owner duplicateEmailSignupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011113333")
-            .build().toEntity();
+            PasswordEncodingDTO passwordEncoder =
+                PasswordEncoder.encodePassword(signupOwner.getPassword());
 
-        Assertions.assertThrows(InvalidParameterException.class,
-            () -> ownerService.signup(duplicateEmailSignupOwner));
-    }
+            Owner owner = Owner.builder()
+                .email(signupOwner.getEmail())
+                .password(passwordEncoder.getEncodePassword())
+                .salt(passwordEncoder.getSalt())
+                .phoneNumber(signupOwner.getPhoneNumber())
+                .build();
 
-    @Test
-    @DisplayName("가게사장 회원가입 실패 - 중복된 휴대폰 번호")
-    void signupFailTest_DuplicatePhoneNumber() {
-        Owner signupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build().toEntity();
+            OwnerSigninDTO signinDTO = OwnerSigninDTO.builder()
+                .email("test@gmail.com")
+                .password("12345678")
+                .build();
 
-        ownerService.signup(signupOwner);
+            HttpSession session = mock(HttpSession.class);
 
-        Owner duplicatePhoneNumberSignupOwner = OwnerSignupDTO.builder()
-            .email("another@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build().toEntity();
+            given(ownerRepository.findUserByEmail(signinDTO.getEmail()))
+                .willReturn(Optional.of(owner));
 
-        Assertions.assertThrows(InvalidParameterException.class,
-            () -> ownerService.signup(duplicatePhoneNumberSignupOwner));
-    }
+            // when
+            ownerService.signin(signinDTO, session);
 
-    @Test
-    @DisplayName("가게사장 로그인 성공 - 세션에 이메일 등록")
-    void signinSuccessTest() {
-        OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build();
-        ownerService.signup(signupOwner.toEntity());
+            // then
+            then(session)
+                .should(times(1))
+                .setAttribute(any(), any(Owner.class));
+        }
 
-        HttpSession session = new MockHttpSession();
-        ownerService.signin(signupOwner.getEmail(), signupOwner.getPassword(), session);
-        assertThat(session.getAttribute("ownerEmail")).isEqualTo(signupOwner.getEmail());
-    }
+        @Test
+        @DisplayName("실패 - 일치하지 않는 이메일")
+        void signinFailTest_invalidEmail() {
+            // given
+            OwnerSigninDTO faliSigninDTO = OwnerSigninDTO.builder()
+                .email("wrong@gmail.com")
+                .password("12345678")
+                .build();
 
-    @Test
-    @DisplayName("가게사장 로그인 실패 - 일치하지 않는 이메일")
-    void signinFailTest_invalidEmail() {
-        OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build();
-        ownerService.signup(signupOwner.toEntity());
+            HttpSession session = mock(HttpSession.class);
 
-        HttpSession session = new MockHttpSession();
-        Assertions.assertThrows(DataNotFoundException.class,
-            () -> ownerService.signin("fail@gmail.com", signupOwner.getPassword(), session));
-    }
+            given(ownerRepository.findUserByEmail(any()))
+                .willReturn(Optional.empty());
 
-    @Test
-    @DisplayName("가게사장 로그인 실패 - 일치하지 않는 패스워드")
-    void signinFailTest_invalidPassword() {
-        OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build();
-        ownerService.signup(signupOwner.toEntity());
+            // when & then
+            assertThatThrownBy(() -> ownerService.signin(faliSigninDTO, session))
+                .isInstanceOf(DataNotFoundException.class)
+                .hasMessage("일치하는 계정을 찾을 수 없습니다.");
+        }
 
-        HttpSession session = new MockHttpSession();
-        Assertions.assertThrows(DataNotFoundException.class,
-            () -> ownerService.signin(signupOwner.getEmail(), "87654321", session));
-    }
+        @Test
+        @DisplayName("실패 - 일치하지 않는 패스워드")
+        void signinFailTest_invalidPassword() {
+            // given
+            OwnerSigninDTO faliSigninDTO = OwnerSigninDTO.builder()
+                .email("seyun@gmail.com")
+                .password("wrongwrong")
+                .build();
 
-    @Test
-    @DisplayName("가게사장 로그아웃 성공")
-    void logoutSuccessTest() {
-        OwnerSignupDTO signupOwner = OwnerSignupDTO.builder()
-            .email("owner@gmail.com")
-            .password("12345678")
-            .phoneNumber("01011111111")
-            .build();
-        ownerService.signup(signupOwner.toEntity());
+            String password = "12345678";
 
-        HttpSession session = new MockHttpSession();
-        ownerService.signin(signupOwner.getEmail(), signupOwner.getPassword(), session);
-        ownerService.logout(session);
-        Assertions.assertThrows(IllegalStateException.class,
-            () -> session.getAttribute(signupOwner.getEmail()));
+            PasswordEncodingDTO passwordEncoder =
+                PasswordEncoder.encodePassword(password);
 
+            Owner owner = Owner.builder()
+                .email("seyun@gmail.com")
+                .password(passwordEncoder.getEncodePassword())
+                .salt(passwordEncoder.getSalt())
+                .phoneNumber("01044445555")
+                .build();
+
+            HttpSession session = mock(HttpSession.class);
+
+            given(ownerRepository.findUserByEmail(faliSigninDTO.getEmail()))
+                .willReturn(Optional.of(owner));
+
+            // when & then
+            assertThatThrownBy(() -> ownerService.signin(faliSigninDTO, session))
+                .isInstanceOf(DataNotFoundException.class)
+                .hasMessage("일치하는 계정을 찾을 수 없습니다.");
+        }
+
+        @Test
+        @DisplayName("로그아웃 성공")
+        void logoutSuccessTest() {
+            // given
+            String password = "12345678";
+
+            PasswordEncodingDTO passwordEncoder =
+                PasswordEncoder.encodePassword(password);
+
+            Owner owner = Owner.builder()
+                .email("seyun@gmail.com")
+                .password(passwordEncoder.getEncodePassword())
+                .salt(passwordEncoder.getSalt())
+                .phoneNumber("01044445555")
+                .build();
+
+            String uuid = UUID.randomUUID().toString();
+            HttpSession session = spy(HttpSession.class);
+            session.setAttribute(uuid, owner);
+
+            // when
+            ownerService.logout(session);
+
+            // then
+            then(session)
+                .should(times(1))
+                .invalidate();
+            assertThat(session.getAttribute(uuid))
+                .isNull();
+        }
     }
 }
