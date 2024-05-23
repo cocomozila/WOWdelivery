e3055fa9ebfb6cca52aea72ccd012c36874486a8
cocomozila <joseyun94@naver.com>
2024-05-23 17:27:16 +0900

    refactor: OwnerService, Repository
    - OwnerService: 받는 파라미터 변경에 따른 메서드 내부 수정

--- src/main/java/com/wow/delivery/repository/OwnerRepository.java
+++ src/main/java/com/wow/delivery/repository/OwnerRepository.java
@@ -1,7 +1,20 @@
 package com.wow.delivery.repository;
 
 import com.wow.delivery.entity.Owner;
+import com.wow.delivery.error.ErrorCode;
+import com.wow.delivery.error.exception.DataNotFoundException;
+
+import java.util.Optional;
 
 public interface OwnerRepository extends CustomJpaRepository<Owner, Long> {
 
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    Optional<Owner> findUserByEmail(String email);
+
+    default Owner getByEmail(String email) {
+        return findUserByEmail(email)
+            .orElseThrow(() -> new DataNotFoundException(ErrorCode.DATA_NOT_FOUND, "일치하는 계정을 찾을 수 없습니다."));
+    }
 }
