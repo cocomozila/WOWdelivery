c5be74f2ac96c48a5cc8968472d8c7061dfe9eb3
cocomozila <joseyun94@naver.com>
2024-06-22 17:29:45 +0900

    refactor: Service 레이어
    ShopService
    - 중복된 로직의 코드 리팩토링

--- src/main/java/com/wow/delivery/dto/shop/CategoryNearbyShopRequestDTO.java
+++ src/main/java/com/wow/delivery/dto/shop/CategoryNearbyShopRequestDTO.java
@@ -6,39 +6,13 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class CategoryNearbyShopRequestDTO {
-
-    private Long userId; // 가게사장 식별번호
+public class CategoryNearbyShopRequestDTO extends NearbyShopRequestDTO {
 
     private String searchCategory; // 카테고리
 
-    private String state; // 주소(도)
-
-    private String city; // 주소(시)
-
-    private String district; // 주소(구,군)
-
-    private String streetName; // 주소(도로명)
-
-    private String buildingNumber; // 주소(빌딩 번호)
-
-    private String addressDetail; // 상세주소
-
-    private Double latitude; // 위도
-
-    private Double longitude; // 경도
-
-    @Builder
+    @Builder(builderMethodName = "categoryNearbyShopRequestDTOBuilder")
     public CategoryNearbyShopRequestDTO(Long userId, String searchCategory, String state, String city, String district, String streetName, String buildingNumber, String addressDetail, Double latitude, Double longitude) {
-        this.userId = userId;
+        super(userId, state, city, district, streetName, buildingNumber, addressDetail, latitude, longitude);
         this.searchCategory = searchCategory;
-        this.state = state;
-        this.city = city;
-        this.district = district;
-        this.streetName = streetName;
-        this.buildingNumber = buildingNumber;
-        this.addressDetail = addressDetail;
-        this.latitude = latitude;
-        this.longitude = longitude;
     }
 }
---
+++ src/main/java/com/wow/delivery/dto/shop/NearbyShopRequestDTO.java
@@ -0,0 +1,33 @@
+package com.wow.delivery.dto.shop;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NearbyShopRequestDTO {
+
+    private Long userId; // 가게사장 식별번호
+    private String state; // 주소(도)
+    private String city; // 주소(시)
+    private String district; // 주소(구,군)
+    private String streetName; // 주소(도로명)
+    private String buildingNumber; // 주소(빌딩 번호)
+    private String addressDetail; // 상세주소
+    private Double latitude; // 위도
+    private Double longitude; // 경도
+
+    @Builder
+    public NearbyShopRequestDTO(Long userId, String state, String city, String district, String streetName, String buildingNumber, String addressDetail, Double latitude, Double longitude) {
+        this.userId = userId;
+        this.state = state;
+        this.city = city;
+        this.district = district;
+        this.streetName = streetName;
+        this.buildingNumber = buildingNumber;
+        this.addressDetail = addressDetail;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
