cc8b084ea7dbb4a1b6634f709d1026fa191447b0
cocomozila <joseyun94@naver.com>
2024-07-03 22:08:19 +0900

    feat: ShopController 기능 추가
    - shop 조회 기능
    - 메뉴 카테고리 추가/수정/순서변경/조회 기능

---
+++ src/main/java/com/wow/delivery/dto/shop/ShopResponse.java
@@ -0,0 +1,52 @@
+package com.wow.delivery.dto.shop;
+
+import com.wow.delivery.dto.menu.MenuResponse;
+import com.wow.delivery.entity.common.Address;
+import com.wow.delivery.entity.shop.BusinessHours;
+import com.wow.delivery.entity.shop.DayOfWeekListConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ShopResponse {
+
+    @Comment(value = "가게 명")
+    private String shopName;
+
+    @Comment(value = "가게 소개")
+    private String introduction;
+
+    @Comment(value = "영업 시간")
+    private BusinessHours businessHours;
+
+    @Comment(value = "주소")
+    private Address address;
+
+    @Column(name = "open_days")
+    @Convert(converter = DayOfWeekListConverter.class)
+    private List<DayOfWeek> openDays = new ArrayList<>();
+
+    @Comment(value = "최소 주문 금액")
+    private int minOrderPrice;
+
+    @Comment(value = "메뉴들")
+    private List<MenuResponse> menus;
+
+    @Builder
+    public ShopResponse(String shopName, String introduction, BusinessHours businessHours, Address address, List<DayOfWeek> openDays, int minOrderPrice, List<MenuResponse> menus) {
+        this.shopName = shopName;
+        this.introduction = introduction;
+        this.businessHours = businessHours;
+        this.address = address;
+        this.openDays = openDays;
+        this.minOrderPrice = minOrderPrice;
+        this.menus = menus;
+    }
+}
