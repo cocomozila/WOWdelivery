b198469d2d59374caf5c314e768550101328f995
cocomozila <joseyun94@naver.com>
2024-07-04 22:59:17 +0900

    refactor: ShopController - 기존 메뉴카테고리 api 진입점 MenuCategoryController로 변경

--- src/main/java/com/wow/delivery/controller/ShopController.java
+++ src/main/java/com/wow/delivery/controller/ShopController.java
@@ -1,11 +1,6 @@
 package com.wow.delivery.controller;
 
-import com.wow.delivery.dto.menu.category.MenuCategoryCreateDTO;
-import com.wow.delivery.dto.menu.category.MenuCategoryOrderUpdateDTO;
-import com.wow.delivery.dto.menu.category.MenuCategoryResponse;
-import com.wow.delivery.dto.menu.category.MenuCategoryUpdateDTO;
 import com.wow.delivery.dto.shop.*;
-import com.wow.delivery.service.MenuCategoryService;
 import com.wow.delivery.service.ShopService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +16,6 @@ import java.util.List;
 public class ShopController {
 
     private final ShopService shopService;
-    private final MenuCategoryService menuCategoryService;
 
     @PostMapping
     public ResponseEntity<HttpStatus> register(@RequestBody @Valid ShopCreateDTO shopCreateDTO) {
@@ -45,31 +39,8 @@ public class ShopController {
     }
 
     @PutMapping
-    public ResponseEntity<HttpStatus> updateAddress(@RequestBody @Valid ShopUpdateDTO shopUpdateDTO) {
+    public ResponseEntity<HttpStatus> updateShop(@RequestBody @Valid ShopUpdateDTO shopUpdateDTO) {
         shopService.updateShop(shopUpdateDTO);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PostMapping("/menu-category")
-    public ResponseEntity<HttpStatus> registerMenuCategory(@RequestBody @Valid MenuCategoryCreateDTO createDTO) {
-        menuCategoryService.createMenuCategory(createDTO);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{shopId}/menu-category")
-    public ResponseEntity<List<MenuCategoryResponse>> getMenuCategory(@PathVariable("shopId") Long shopId) {
-        return ResponseEntity.ok(menuCategoryService.getMenuCategory(shopId));
-    }
-
-    @PutMapping("/menu-category/reorder")
-    public ResponseEntity<HttpStatus> reorderMenuCategories(@RequestBody @Valid MenuCategoryOrderUpdateDTO updateDTO) {
-        menuCategoryService.reorderMenuCategories(updateDTO);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PutMapping("/menu-category")
-    public ResponseEntity<HttpStatus> reorderMenuCategories(@RequestBody @Valid MenuCategoryUpdateDTO updateDTO) {
-        menuCategoryService.updateMenuCategory(updateDTO);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
