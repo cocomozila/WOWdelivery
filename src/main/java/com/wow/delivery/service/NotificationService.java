1e082940c148a5d914b73a45c78ee2a154e3965a
cocomozila <joseyun94@naver.com>
2024-08-17 22:03:40 +0900

    feat: NotificationService 추가

---
+++ src/main/java/com/wow/delivery/service/NotificationService.java
@@ -0,0 +1,28 @@
+package com.wow.delivery.service;
+
+import com.wow.delivery.dto.order.OrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NotificationService {
+
+    public void sendAppNotification(OrderResponse orderResponse) {
+        // 비동기로 작성
+        // 앱으로 알림 전송
+        log.info("해당 매장으로 주문생성 알림 전송 : {}", orderResponse.getShopId());
+    }
+
+    public void sendKakaoAcceptOrderNotification(Long shopId) {
+        // 비동기로 작성
+        // 앱으로 알림 전송
+        log.info("해당 유저에게 주문 성공 카카오 알림 전송 : {}", shopId);
+    }
+
+    public void sendKakaoRejectOrderNotification(Long shopId) {
+        // 비동기로 작성
+        // 앱으로 알림 전송
+        log.info("해당 유저에게 주문 실패 카카오 알림 전송 : {} ", shopId);
+    }
+}
