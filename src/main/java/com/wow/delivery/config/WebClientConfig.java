2e850c471cb976d147ec1bc0d9bad08fcee96d5a
cocomozila <joseyun94@naver.com>
2024-08-17 22:16:43 +0900

    feat: WebClientConfig tossPayment 헤더 설정 추가

--- src/main/java/com/wow/delivery/config/WebClientConfig.java
+++ src/main/java/com/wow/delivery/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.wow.delivery.config;
 
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -9,7 +10,9 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -24,6 +27,27 @@ public class WebClientConfig {
 
         return WebClient.builder()
             .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+    }
+
+    @Bean
+    public WebClient tossPaymentWebClient(
+        @Value("${test.toss.secret.api.key}")
+        String TEST_TOSS_SECRET_API_KEY
+    ) {
+        HttpClient httpClient = HttpClient.create()
+            .responseTimeout(Duration.ofMillis(5000)) // 설정 시간내 응답이 없으면 timeout 발생
+            .doOnConnected(conn ->
+                conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))); // 읽기 작업 타임아웃을 설정
+
+        String encodedAuth = Base64.getEncoder()
+            .encodeToString(TEST_TOSS_SECRET_API_KEY.getBytes(StandardCharsets.UTF_8));
+
+        return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuth)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .build();
     }
 }
