b492fc5cd79b0fdb6e5525be6a06e7fb09b465a2
Cho seyun <joseyun94@naver.com>
2024-09-03 21:10:49 +0900

    feat: 로컬캐시(Caffeine Cache)를 글로벌(Redis)캐시로 변경

--- src/main/java/com/wow/delivery/config/RedisConfig.java
+++ src/main/java/com/wow/delivery/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.wow.delivery.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -19,7 +18,7 @@ public class RedisConfig {
     private int redisPort;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public LettuceConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
         redisStandaloneConfiguration.setPort(redisPort);
