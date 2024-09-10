e3daef9ca6bb4d929db662dabe2ff9c3510d364b
Cho seyun <joseyun94@naver.com>
2024-09-10 15:16:09 +0900

    feat: WebConfig token Filter 삭제

--- src/main/java/com/wow/delivery/config/WebConfig.java
+++ src/main/java/com/wow/delivery/config/WebConfig.java
@@ -3,8 +3,6 @@ package com.wow.delivery.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wow.delivery.config.filter.ExceptionHandlerFilter;
 import com.wow.delivery.config.filter.LoginFilter;
-import com.wow.delivery.config.filter.WarmupTokenFilter;
-import com.wow.delivery.warmup.WarmupTokenProvider;
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -16,21 +14,11 @@ import org.springframework.context.annotation.Configuration;
 public class WebConfig {
 
     private final ObjectMapper objectMapper;
-    private final WarmupTokenProvider warmupTokenProvider;
 
     @Bean
     public FilterRegistrationBean loginFilter() {
         FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
         filter.setFilter(new LoginFilter());
-        filter.setOrder(3);
-        filter.addUrlPatterns("/*");
-        return filter;
-    }
-
-    @Bean
-    public FilterRegistrationBean warmupTokenFilter() {
-        FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
-        filter.setFilter(new WarmupTokenFilter(warmupTokenProvider));
         filter.setOrder(2);
         filter.addUrlPatterns("/*");
         return filter;
